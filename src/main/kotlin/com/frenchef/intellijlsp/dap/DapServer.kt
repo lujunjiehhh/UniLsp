faaaa5e700a9aefd706442db4463a03d012676b3
SoothSayer <q15182574345@163.com>
2026-01-07 22:01:27 +0800

    fix(dap): import coroutine cancel

--- src/main/kotlin/com/frenchef/intellijlsp/dap/DapServer.kt
+++ src/main/kotlin/com/frenchef/intellijlsp/dap/DapServer.kt
@@ -21,6 +21,7 @@ import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
